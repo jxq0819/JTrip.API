21d01cf449387d387352b043fea33fc6e9173aba
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-23 23:43:10 +0800

    Add ITouristRouteRepository interface

---
+++ JTrip.API/Services/ITouristRouteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JTrip.API.Models;
+
+namespace JTrip.API.Services
+{
+    public interface ITouristRouteRepository
+    {
+        IEnumerable<TouristRoute> GetTouristRoutes();
+        TouristRoute GetTouristRoute(Guid touristRouteId);
+    }
+}
