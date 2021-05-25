844cfc47f021e40a61fbdce585338c09c7294aa7
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-25 21:45:07 +0800

    Add TouristRoutesController

---
+++ JTrip.API/Controllers/TouristRoutesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JTrip.API.Services;
+
+namespace JTrip.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TouristRoutesController : ControllerBase
+    {
+        private ITouristRouteRepository _touristRouteRepository;
+
+        public TouristRoutesController(ITouristRouteRepository touristRouteRepository)
+        {
+            _touristRouteRepository = touristRouteRepository;
+        }
+
+        public IActionResult GetTouristRoutes()
+        {
+            var routes = _touristRouteRepository.GetTouristRoutes();
+            return Ok(routes);
+        }
+    }
+}
