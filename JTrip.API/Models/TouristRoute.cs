84cfd05daa3fa8368cc1611420dfdb1bd24300ed
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-25 23:56:46 +0800

    Add annotations for data models

--- JTrip.API/Models/TouristRoute.cs
+++ JTrip.API/Models/TouristRoute.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,17 +9,27 @@ namespace JTrip.API.Models
 {
     public class TouristRoute
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal OriginalPrice { get; set; }
+        [Range(0.0, 1.0)]
         public double? DiscountPercent { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime? UpdateTime { get; set; }
         public DateTime? DepartureTime { get; set; }
+        [MaxLength]
         public string Features { get; set; }
+        [MaxLength]
         public string Fees { get; set; }
+        [MaxLength]
         public string Notes { get; set; }
-        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; }
+        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; } = new List<TouristRoutePicture>();
     }
 }
