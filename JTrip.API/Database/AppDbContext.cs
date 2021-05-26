e957a77701a8d8c787e2506a40b98f18cabbc71f
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-26 15:44:35 +0800

    Create DataSeeding

--- JTrip.API/Database/AppDbContext.cs
+++ JTrip.API/Database/AppDbContext.cs
@@ -15,5 +15,17 @@ namespace JTrip.API.Database
 
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            {
+                Id = Guid.NewGuid(),
+                Title = "TestTitle",
+                Description = "TestDescription",
+                OriginalPrice = 0,
+                CreateTime = DateTime.UtcNow
+            });
+        }
     }
 }
---
+++ JTrip.API/Migrations/20210526073727_DataSeeding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JTrip.API.Migrations
+{
+    public partial class DataSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreateTime", "DepartureTime", "Description", "DiscountPercent", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("b908ddb3-78ee-46a7-bb49-cc8c67aac28a"), new DateTime(2021, 5, 26, 7, 37, 27, 306, DateTimeKind.Utc).AddTicks(335), null, "TestDescription", null, null, null, null, 0m, "TestTitle", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("b908ddb3-78ee-46a7-bb49-cc8c67aac28a"));
+        }
+    }
+}
