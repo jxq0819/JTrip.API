e7e6298e13684a138a6e7ef1fae92b820aa183ad
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-26 21:35:32 +0800

    Handle unacceptable Accept header of HTTP requests

--- JTrip.API/Startup.cs
+++ JTrip.API/Startup.cs
@@ -27,7 +27,7 @@ namespace JTrip.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options => { options.ReturnHttpNotAcceptable = true; });
             services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
             services.AddDbContext<AppDbContext>(optionsAction =>
             {
