63907101e358b52458f1ded7a0e60a7c1ce61418
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-27 09:59:05 +0800

    Add TouristRoutePicturesController

---
+++ JTrip.API/Controllers/TouristRoutePicturesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using JTrip.API.Services;
+
+namespace JTrip.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TouristRoutePicturesController : ControllerBase
+    {
+        private ITouristRouteRepository _touristRouteRepository;
+        private IMapper _mapper;
+
+        public TouristRoutePicturesController(ITouristRouteRepository touristRouteRepository, IMapper mapper)
+        {
+            _touristRouteRepository =
+                touristRouteRepository ?? throw new ArgumentNullException(nameof(touristRouteRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
