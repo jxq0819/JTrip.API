86ff46d54d8ee1e4114f0d7da5e2835dc931bb38
Junxian Qi <junxian.j.qi@gmail.com>
2021-05-30 16:52:19 +0800

    Encapsulate TouristRouteForUpdateDto and TouristRouteForCreationDto into TouristRouteForManipulationDto

--- JTrip.API/Dtos/TouristRouteForUpdateDto.cs
+++ JTrip.API/Dtos/TouristRouteForUpdateDto.cs
@@ -6,27 +6,10 @@ using System.Threading.Tasks;
 
 namespace JTrip.API.Dtos
 {
-    public class TouristRouteForUpdateDto
+    public class TouristRouteForUpdateDto : TouristRouteForManipulationDto
     {
-        [Required(ErrorMessage = "Title must not be null")]
+        [Required(ErrorMessage = "Description is required for update")]
         [MaxLength(100)]
-        public string Title { get; set; }
-        [Required]
-        [MaxLength(1500)]
-        public string Description { get; set; }
-        // Price is calculated as OriginalPrice * DiscountPercent
-        public decimal Price { get; set; }
-        public DateTime CreateTime { get; set; }
-        public DateTime? UpdateTime { get; set; }
-        public DateTime? DepartureTime { get; set; }
-        public string Features { get; set; }
-        public string Fees { get; set; }
-        public string Notes { get; set; }
-        public double? Rating { get; set; }
-        public string TravelDays { get; set; }
-        public string TripType { get; set; }
-        public string DepartureCity { get; set; }
-        public ICollection<TouristRoutePictureForCreationDto> TouristRoutePictures { get; set; } =
-            new List<TouristRoutePictureForCreationDto>();
+        public override string Description { get; set; }
     }
 }
--- JTrip.API/ValidationAttributes/TouristRouteTitleMustBeDifferentFromDescriptionAttribute.cs
+++ JTrip.API/ValidationAttributes/TouristRouteTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -11,11 +11,11 @@ namespace JTrip.API.ValidationAttributes
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var touristRouteDto = (TouristRouteForCreationDto) validationContext.ObjectInstance;
+            var touristRouteDto = (TouristRouteForManipulationDto) validationContext.ObjectInstance;
             if (touristRouteDto.Title == touristRouteDto.Description)
             {
                 return new ValidationResult("Title and description must be different",
-                    new[] {"TouristRouteForCreationDto"});
+                    new[] {"TouristRouteForManipulationDto"});
             }
 
             return ValidationResult.Success;
