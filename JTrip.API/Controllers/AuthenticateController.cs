d1da395a872778b3845479becc424da360d918a4
Junxian Qi <junxian.j.qi@gmail.com>
2021-06-03 17:54:37 +0800

    Add Login API with JWT to achieve stateless authentication

---
+++ JTrip.API/Controllers/AuthenticateController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JTrip.API.Dtos;
+
+namespace JTrip.API.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticateController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto loginDto)
+        {
+            var signingAlgorithm = SecurityAlgorithms.HmacSha256;
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "fake_user_id")
+            };
+            var secretByte = Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]);
+            var signingKey = new SymmetricSecurityKey(secretByte);
+            var signingCredentials = new SigningCredentials(signingKey, signingAlgorithm);
+            var token = new JwtSecurityToken(issuer: _configuration["Authentication:Issuer"],
+                audience: _configuration["Authentication:Audience"], claims, notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(1), signingCredentials);
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(tokenString);
+        }
+    }
+}
