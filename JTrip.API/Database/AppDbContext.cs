e48231ade3ebd79e7020cf5cba31154b85283dea
Junxian Qi <junxian.j.qi@gmail.com>
2021-06-07 16:02:32 +0800

    Use IdentityDbContext

--- JTrip.API/Database/AppDbContext.cs
+++ JTrip.API/Database/AppDbContext.cs
@@ -4,13 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using JTrip.API.Models;
 
 namespace JTrip.API.Database
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
