45967e4222bf9dc40703bbdba0275b37cde3021a
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-03 00:27:33 +0800

    Create ApplicationUserMigration

---
+++ JTrip.API/Migrations/20210702162541_ApplicationUserMigration.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JTrip.API.Migrations
+{
+    public partial class ApplicationUserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserTokens",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserLogins",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserClaims",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "028ed6ba-9476-468f-82d3-f696554cb3d9", "61931476-a8b7-4880-91e9-0be032ce24e7", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Address", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "30bedf60-427b-49cd-a521-97a3f0a783a7", 0, null, "27669165-9c99-4a0d-9358-8509e0c57fa3", "admin@jtrip.com", true, false, null, "ADMIN@JTRIP.COM", "ADMIN@JTRIP.COM", "AQAAAAEAACcQAAAAEJO8Bqdy87Ma49GxQfnte6zxsMgio9jebjGEFYr0i1MeNkGMS/KqU6rPagonWNQtPQ==", "0123456789", false, "18a783e5-b2f5-428c-bb93-e99f361842ef", false, "admin@jtrip.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "UserId", "RoleId", "ApplicationUserId" },
+                values: new object[] { "30bedf60-427b-49cd-a521-97a3f0a783a7", "028ed6ba-9476-468f-82d3-f696554cb3d9", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserTokens_ApplicationUserId",
+                table: "AspNetUserTokens",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_ApplicationUserId",
+                table: "AspNetUserRoles",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_ApplicationUserId",
+                table: "AspNetUserLogins",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_ApplicationUserId",
+                table: "AspNetUserClaims",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserClaims",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserLogins",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserRoles",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserTokens",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserTokens_ApplicationUserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserLogins_ApplicationUserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserClaims_ApplicationUserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "UserId", "RoleId" },
+                keyValues: new object[] { "30bedf60-427b-49cd-a521-97a3f0a783a7", "028ed6ba-9476-468f-82d3-f696554cb3d9" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "028ed6ba-9476-468f-82d3-f696554cb3d9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "30bedf60-427b-49cd-a521-97a3f0a783a7");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserClaims");
+        }
+    }
+}
