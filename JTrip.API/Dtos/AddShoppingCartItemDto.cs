063f18c465c7352c48c59cfb32f237e9e8f9858f
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-05 17:11:47 +0800

    Add AddShoppingCartItemDto

---
+++ JTrip.API/Dtos/AddShoppingCartItemDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JTrip.API.Dtos
+{
+    public class AddShoppingCartItemDto
+    {
+        public Guid TouristRouteId { get; set; }
+    }
+}
