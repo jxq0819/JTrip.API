a8f332f002af5aad7605b82a0061a4680abc96fd
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-07 16:08:11 +0800

    Add Order model

---
+++ JTrip.API/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JTrip.API.Models
+{
+    public enum OrderStateEnum
+    {
+        Pending,
+        Processing,
+        Completed,
+        Declined,
+        Cancelled,
+        Refund
+    }
+
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public ICollection<LineItem> OrderItems { get; set; }
+        public OrderStateEnum State { get; set; }
+        public DateTime CreateDateUtc { get; set; }
+        public string TransactionMetadata { get; set; }
+    }
+}
