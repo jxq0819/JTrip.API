2dcb7edd15658f14b755d28114a532e8999a93ec
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-12 19:33:17 +0800

    Use state machine in Order model

--- JTrip.API/Models/Order.cs
+++ JTrip.API/Models/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Stateless;
 
 namespace JTrip.API.Models
 {
@@ -16,8 +17,22 @@ namespace JTrip.API.Models
         Refund
     }
 
+    public enum OrderStateTriggerEnum
+    {
+        PlaceOrder,
+        Approve,
+        Reject,
+        Cancel,
+        Return
+    }
+
     public class Order
     {
+        public Order()
+        {
+            StateMachineInit();
+        }
+
         [Key]
         public Guid Id { get; set; }
         public string UserId { get; set; }
@@ -26,5 +41,40 @@ namespace JTrip.API.Models
         public OrderStateEnum State { get; set; }
         public DateTime CreateDateUtc { get; set; }
         public string TransactionMetadata { get; set; }
+        private StateMachine<OrderStateEnum, OrderStateTriggerEnum> _machine;
+
+        private void StateMachineInit()
+        {
+            _machine = new StateMachine<OrderStateEnum, OrderStateTriggerEnum>(() => State, s => State = s);
+
+            _machine.Configure(OrderStateEnum.Pending)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Cancel, OrderStateEnum.Cancelled);
+
+            _machine.Configure(OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Approve, OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Reject, OrderStateEnum.Declined);
+
+            _machine.Configure(OrderStateEnum.Declined)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing);
+
+            _machine.Configure(OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Return, OrderStateEnum.Refund);
+        }
+
+        public void PaymentProcessing()
+        {
+            _machine.Fire(OrderStateTriggerEnum.PlaceOrder);
+        }
+
+        public void PaymentApprove()
+        {
+            _machine.Fire(OrderStateTriggerEnum.Approve);
+        }
+
+        public void PaymentReject()
+        {
+            _machine.Fire(OrderStateTriggerEnum.Reject);
+        }
     }
 }
