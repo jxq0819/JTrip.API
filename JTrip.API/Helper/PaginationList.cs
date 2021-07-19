63fed03dcff8b95eab1164af5c2126a498450218
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-19 19:36:40 +0800

    Add PaginationList

---
+++ JTrip.API/Helper/PaginationList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace JTrip.API.Helper
+{
+    public class PaginationList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+
+        public PaginationList(int currentPage, int pageSize, List<T> items)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            AddRange(items);
+        }
+
+        public static async Task<PaginationList<T>> CreateAsync(int currentPage, int pageSize, IQueryable<T> result)
+        {
+            var skip = (currentPage - 1) * pageSize;
+            result = result.Skip(skip);
+            result = result.Take(pageSize);
+            var items = await result.ToListAsync();
+            return new PaginationList<T>(currentPage, pageSize, items);
+        }
+    }
+}
