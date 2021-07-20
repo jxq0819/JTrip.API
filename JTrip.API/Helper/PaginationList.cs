9ffea08fa74d051e4cf2f40dddbee429f9917cd2
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-20 11:23:40 +0800

    Add more attributes in PaginationList

--- JTrip.API/Helper/PaginationList.cs
+++ JTrip.API/Helper/PaginationList.cs
@@ -8,23 +8,30 @@ namespace JTrip.API.Helper
 {
     public class PaginationList<T> : List<T>
     {
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
         public int CurrentPage { get; set; }
         public int PageSize { get; set; }
 
-        public PaginationList(int currentPage, int pageSize, List<T> items)
+        public PaginationList(int totalCount, int currentPage, int pageSize, List<T> items)
         {
+            TotalCount = totalCount;
             CurrentPage = currentPage;
             PageSize = pageSize;
+            TotalPages = (int) Math.Ceiling(totalCount / (double) pageSize);
             AddRange(items);
         }
 
         public static async Task<PaginationList<T>> CreateAsync(int currentPage, int pageSize, IQueryable<T> result)
         {
+            var totalCount = await result.CountAsync();
             var skip = (currentPage - 1) * pageSize;
             result = result.Skip(skip);
             result = result.Take(pageSize);
             var items = await result.ToListAsync();
-            return new PaginationList<T>(currentPage, pageSize, items);
+            return new PaginationList<T>(totalCount, currentPage, pageSize, items);
         }
     }
 }
