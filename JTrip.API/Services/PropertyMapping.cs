2f7a56543941014b2798d8db93ca93af8eb97f1c
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-25 17:19:13 +0800

    Add PropertyMapping

---
+++ JTrip.API/Services/PropertyMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JTrip.API.Services
+{
+    public class PropertyMapping<TSource, TDestination> : IPropertyMapping
+    {
+        public Dictionary<string, PropertyMappingValue> MappingDictionary { get; set; }
+
+        public PropertyMapping(Dictionary<string, PropertyMappingValue> mappingDictionary)
+        {
+            MappingDictionary = mappingDictionary;
+        }
+    }
+}
