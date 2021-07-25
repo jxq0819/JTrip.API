88493df4aeb43d2782d2adddcb5291a7df524f1d
Junxian Qi <junxian.j.qi@gmail.com>
2021-07-26 00:22:02 +0800

    Add IQueryableExtensions

---
+++ JTrip.API/Helper/IQueryableExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using JTrip.API.Services;
+
+namespace JTrip.API.Helper
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> ApplySort<T>(
+            this IQueryable<T> source,
+            string orderBy,
+            Dictionary<string, PropertyMappingValue> mappingDictionary)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (mappingDictionary == null)
+            {
+                throw new ArgumentNullException(nameof(mappingDictionary));
+            }
+
+            if (string.IsNullOrWhiteSpace(orderBy))
+            {
+                return source;
+            }
+
+            var orderByString = string.Empty;
+            var orderByAfterSplit = orderBy.Split(",");
+            foreach (var order in orderByAfterSplit)
+            {
+                var trimmedOrder = order.Trim();
+                var orderDescending = trimmedOrder.EndsWith(" desc");
+                var indexOfFirstSpace = trimmedOrder.IndexOf(" ", StringComparison.Ordinal);
+                var propertyName = indexOfFirstSpace == -1 ? trimmedOrder : trimmedOrder.Remove(indexOfFirstSpace);
+                if (!mappingDictionary.ContainsKey(propertyName))
+                {
+                    throw new ArgumentException($"Key mapping for {propertyName} is missing");
+                }
+
+                var propertyMappingValue = mappingDictionary[propertyName];
+                if (propertyMappingValue == null)
+                {
+                    throw new ArgumentNullException(nameof(propertyMappingValue));
+                }
+
+                foreach (var destinationProperty in propertyMappingValue.DestinationProperties.Reverse())
+                {
+                    orderByString = orderByString + (string.IsNullOrWhiteSpace(orderByString) ? string.Empty : ", ") +
+                                    destinationProperty + (orderDescending ? " descending" : " ascending");
+                }
+            }
+
+            return source.OrderBy(orderByString);
+        }
+    }
+}
